bff832f126965cd3f2b52743c03f34b0cf4e3007
UlyssePerret <ulysseperret@orange.fr>
2022-01-16 17:30:28 +0100

    ajout dossier test - for vue

--- devtips/src/main.js
+++ devtips/src/main.js
@@ -1,11 +1,12 @@
-import { createApp } from 'vue'
+ import { createApp } from 'vue'
 import App from './App.vue'
-import Vue from 'vue'
+//import Vue from 'vue'
 
-Vue.config.procutionTip = false
+//Vue.config.procutionTip = false
 
+/**
 new Vue({
     render: h => h(App),
 }).$mount('#app')
-
+ */
 createApp(App).$mount('#app')
