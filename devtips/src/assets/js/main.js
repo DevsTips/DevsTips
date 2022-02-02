738c87b44c82c6fec1c9932d2b78fa444f40eb99
Ulysse Perret <ulysseperret@orange.fr>
2022-02-02 13:56:37 +0100

    Ajout bootstrap?

--- devtips/src/assets/js/main.js
+++ devtips/src/assets/js/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from '../../App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import '../style/bootstrap-4.1.3-dist/css/bootstrap.min.css'
+//©import '../dist/css/bootstrap.min.css'
 
 new Vue({
     render: h => h(App),
 }).$mount('#app')
 
- createApp(App).mount('#app')
+createApp(App).mount('#app')
