e34de206e2182ef09440d846b4744bd0a98c0bb2
UlyssePerret <ulysseperret@orange.fr>
2022-02-02 23:02:36 +0100

    Status change

--- devtips/src/main.js
+++ devtips/src/main.js
@@ -9,9 +9,4 @@ new Vue({
     render: h => h(App), c
 }).$mount('#app')
 */
-<<<<<<< HEAD
  createApp(App).mount('#app') 
-=======
- createApp(App).mount('#app')
- 
->>>>>>> 9a9f8be46a785c055d217068c253f0ac5ae41f57
