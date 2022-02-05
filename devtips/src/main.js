9bc39474b4a547bd1d60c8ec637a280103203ac5
Ulysse Perret <ulysseperret@orange.fr>
2022-02-05 18:09:37 +0100

    Verif 2éme mac

--- devtips/src/main.js
+++ devtips/src/main.js
@@ -1,12 +1,10 @@
 import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
- 
-// import Vue from 'vue'
-import "../dist/css/bootstrap.min.css"
+import '../src/assets/style/bootstrap-4.1.3-dist/css/bootstrap.min.css'
 
-/** 
 new Vue({
-    render: h => h(App), c
+    render: h => h(App),
 }).$mount('#app')
-*/
- createApp(App).mount('#app') 
+
+createApp(App).mount('#app') 
