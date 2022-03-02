a61cc311864c25718be7b1d0a6a74140db4bc43b
UlyssePerret <ulysseperret@orange.fr>
2022-03-02 22:05:34 +0100

    point remise ajout component

---
+++ devtips/devstips.sql
@@ -0,0 +1,60 @@
+<template>
+  <div class="card-margin">
+    <div class="row">
+      <div class="col-sm-3">
+        <div class="card">
+          <img src="@/assets/variants.png" alt="Image de l'article" />
+          <div class="card-body">
+            <h5 class="card-title">
+              Comprendre et créer des variants sur Figma
+            </h5>
+            <p>3 minutes de lecture</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="card">
+          <img src="@/assets/variants.png" alt="Image de l'article" />
+          <div class="card-body">
+            <h5 class="card-title">
+              Comprendre et créer des variants sur Figma
+            </h5>
+            <p>3 minutes de lecture</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="card">
+          <img src="@/assets/variants.png" alt="Image de l'article" />
+          <div class="card-body">
+            <h5 class="card-title">
+              Comprendre et créer des variants sur Figma
+            </h5>
+            <p>3 minutes de lecture</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+div {
+  background-color: #191e2a;
+  text-align: left;
+}
+.card-margin {
+  margin-left: 200px;
+  margin-top: 50px;
+}
+.row {
+  margin-right: O;
+}
+.col-sm-3 {
+  margin-right: 100px;
+}
+.card {
+  width: 272px;
+  height: 344px;
+}
+</style>
---
+++ devtips/src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <p>Credit : Baptiste Dantas, Nikolla Marinkovic, Ulysse Perret
+            2022
+        </p>
+    </div>
+</template>
