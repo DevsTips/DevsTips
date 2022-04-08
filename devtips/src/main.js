8078545acaccfb12b499ee106c4e82a4ab544327
UlyssePerret <ulysseperret@orange.fr>
2022-04-08 11:22:22 +0200

    mise a jour ?

--- devtips/src/main.js
+++ devtips/src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+//  import { createApp } from 'vue'
 import Vue from 'vue'
 import App from './App.vue'
 import '../src/assets/style/bootstrap-4.1.3-dist/css/bootstrap.min.css'
@@ -7,4 +7,4 @@ new Vue({
     render: h => h(App),
 }).$mount('#app')
 
-//createApp(App).mount('#app') 
+// createApp(App).mount('#app') 
