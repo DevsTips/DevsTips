3f7c1c585ec72f929d5d191d51e7228022487793
UlyssePerret <ulysseperret@orange.fr>
2022-04-13 08:56:20 +0200

    verif windows

--- devtips/src/components/Footer.vue
+++ devtips/src/components/Footer.vue
@@ -1,9 +1,5 @@
 <template>
     <div>
-<<<<<<< HEAD
         <p>Fait par Baptiste Dantas, Nikola Marinkovic et Ulysse Perret</p>
-=======
-        <p>Fait avec ❤️</p>
->>>>>>> Mise a jour- ajout fichiers
     </div>
 </template>
