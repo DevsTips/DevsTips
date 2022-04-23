b2ab9d1ac11a38b1e0825ab9be77c5b8b026c332
Ulysse Perret <ulysseperret@orange.fr>
2022-04-23 14:44:43 +0200

    checking

--- devtips/src/components/Footer.vue
+++ devtips/src/components/Footer.vue
@@ -1,11 +1,5 @@
 <template>
-<<<<<<< HEAD
-    <div>
-        <p>Fait par Baptiste Dantas, Nikola Marinkovic et Ulysse Perret</p>
-    </div>
-=======
   <div>
     <p>Fait par Baptiste Dantas, Nikola Marinkovic et Ulysse Perret</p>
   </div>
->>>>>>> test
 </template>
