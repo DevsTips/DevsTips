65ef7063f44c3946963aa0829ac925ad7bc77fda
Ulysse Perret <ulysseperret@orange.fr>
2022-05-24 07:52:06 +0200

    test git (a ne pas merge !)

--- devtips/src/components/Contact.vue
+++ devtips/src/components/Contact.vue
@@ -2,9 +2,9 @@
         Voici les differents moyens de contacts
         <div class="container">
             <p>
-            Baptiste Dantas : 
-            Nicolla Marinkovic :
-            Ulysse Perret : 
+            Baptiste Dantas : baptistedantas@orange.fr
+            Nicolla Marinkovic : nikola.marinkovic.ort@gmail.com
+            Ulysse Perret : ulysseperret@orange.fr
             </p>
             
         </div>
---
+++ symfony_Devstips-up/utilisateur.php
@@ -0,0 +1,16 @@
+<?php
+
+// Premier utilisateur
+$userName1 = 'Ulysse Perret';
+$userEmail1 = 'ulysseperret@orange.com';
+$userPassword1 = 'Devstips1234';
+
+// Deuxième utilisatrice
+$userName2 = 'Baptiste Dantas';
+$userEmail2 = 'baptistedantas@orange.fr';
+$userPassword1 = 'Devstips1234';
+
+// 3éme utilisatrice
+$userName2 = 'Nickolla Marinkovic';
+$userEmail2 = 'nikola.marinkovic.ort@gmail.com';
+$userPassword1 = 'Devstips1234';
